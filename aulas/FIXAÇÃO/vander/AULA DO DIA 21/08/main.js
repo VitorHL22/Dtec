d77f8cf0dcbfcff119ca55ca74814131ff71c06b
Vitor hugo <vitor.ferreira.tec@despertar.org.br>
2025-08-28 13:40:00 -0300

    fixação

---
+++ aulas/FIXAÇÃO/vander/AULA DO DIA 21/08/main.js
@@ -0,0 +1,67 @@
+const lista = {
+    "nome": "Lista de compras",
+    "data": new Date,
+    "itens": [
+        {
+        "item": "Maça",
+        "quantidade": 5
+        },
+        {
+            "item": "pão",
+            "quantidade": 10
+        }
+    ]
+}
+console.log(lista.data)
+console.log(lista.itens[0].item)
+// 
+const perfilUsuario ={
+    "nome": "Vitor",
+    "dataDeNascimento": "05/08/2007",
+    "email": "vitorhugo@gmail.com",
+    "hobies":[
+        {
+        "hobie": "jogar bola",
+        "dia": "10/08/2025"
+        },
+        {
+        "hobie": "jogar jogos",
+        "dia": "11/08/2025"
+        },
+        {
+        "hobie": "mecher no celular",
+        "dia": "12/08/2025"
+        },
+        {
+        "hobie": "programar",
+        "dia": "13/08/2025"
+        },
+        {
+        "hobie": "estudar",
+        "dia": "14/08/2025"
+        }
+    ]
+}
+// 
+const catalogo ={
+    "livros":[
+        {
+        "titulo": "harry Potter",
+        "autor": "J. K. Rowling",
+        "ano": 1998,
+        "disponivel": true
+        },
+        {
+        "titulo": "Percy Jackson",
+        "autor": "Rick Riondan",
+        "ano": 2005,
+        "disponivel": true
+        },
+        {
+        "titulo": "Lupan",
+        "autor": "Maurice Leblanc",
+        "ano": 1907,
+        "disponivel": true
+        },
+    ]
+}
---
+++ aulas/FIXAÇÃO/vander/main.js
@@ -0,0 +1,8 @@
+// 1
+function mostrar(){
+    const h1 = document.querySelector("#titulo").textContent
+    alert(h1)
+}
+// 2
+const p = document.querySelector(".texto")
+p.textContent = "Texto alterado"
